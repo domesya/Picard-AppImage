8948d2b0bc4be31b7bb6be30f48322fca904bc5c
domesya <163451587+domesya@users.noreply.github.com>
2026-09-25 13:48:58 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=usr/share/icons/hicolor/scalable/apps/org.musicbrainz.Picard.svg
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.musicbrainz.Picard.svg
 export DESKTOP=/usr/share/applications/org.musicbrainz.Picard.desktop
 export STARTUPWMCLASS=org.musicbrainz.Picard.desktop
 
